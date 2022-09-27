ef8bcc8453ab4fc22ac233e7a5e761056df21924
MatheusP0104 <105940986+MatheusP0104@users.noreply.github.com>
2022-09-27 08:42:53 -0300

    Create db_formulario.sql

---
+++ eventoSenac/db_formulario.sql
@@ -0,0 +1,8 @@
+create database formulario;
+use formulario;
+
+create table usuarios (id int auto_increment primary key, nome varchar(45), email varchar(45), telefone varchar(15),idade varchar(2), sexo varchar(30), curso varchar(100));
+select * from usuarios;
+
+#drop database formulario;
+
